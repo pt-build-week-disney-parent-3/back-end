cc6889e911b3ca5350bf4e25616e68d89737b5de
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-27 15:41:34 -0500

    Parent Schema Created

--- database/migrations/20200227152644_parents.js
+++ database/migrations/20200227152644_parents.js
@@ -1,8 +1,18 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("parents", parents => {
+
+    parents.increments("parent_id")
+
+    parents.integer("user_id")
+      .notNullable()
+      .references("user_id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("parents")
 };
