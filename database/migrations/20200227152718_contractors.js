d8a1b8947e7441f04bcc2aaa33b7e70b7709d994
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-27 15:50:25 -0500

    Contractors Schema Created

--- database/migrations/20200227152718_contractors.js
+++ database/migrations/20200227152718_contractors.js
@@ -1,8 +1,21 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("contractors", contractors => {
+
+    contractors.increments("contractor_id")
+
+    contractors.string("price", 128)
+      .notNullable()
+
+    contractors.integer("user_id")
+      .notNullable()
+      .references("user_id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("contractors")
 };
