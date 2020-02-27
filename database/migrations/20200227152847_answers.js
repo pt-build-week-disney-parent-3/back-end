7f3a5f4e8a4e9378f1e77732211bf00e8bfbc8c8
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-27 16:16:01 -0500

    Answers Schema Created

--- database/migrations/20200227152847_answers.js
+++ database/migrations/20200227152847_answers.js
@@ -1,8 +1,37 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("answers", answers => {
+
+    answers.increments("answer_id")
+
+    answers.string("username", 128)
+      .notNullable()
+
+    answers.string("first_name", 128)
+      .notNullable()
+
+    answers.string("last_name", 128)
+      .notNullable()
+
+    answers.string("answer", 500)
+      .notNullable()
+
+    answers.integer("question_id")
+      .notNullable()
+      .references("question_id")
+      .inTable("questions")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+
+    answers.integer("user_id")
+      .notNullable()
+      .references("user_id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("answers")
 };
