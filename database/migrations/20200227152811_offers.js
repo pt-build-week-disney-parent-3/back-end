8ba32ec8404a7297e264ae3ce231b411bb27b333
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-27 16:05:48 -0500

    Offers Schema Created

--- database/migrations/20200227152811_offers.js
+++ database/migrations/20200227152811_offers.js
@@ -1,8 +1,39 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("offers", offers => {
+
+    offers.increments("offer_id")
+
+    offers.string("username", 128)
+      .notNullable()
+
+    offers.string("first_name", 128)
+      .notNullable()
+
+    offers.string("last_name", 128)
+      .notNullable()
+
+    offers.string("offer", 500)
+      .notNullable()
+
+    offers.string("max_child_count", 128)
+      .notNullable()
+
+    offers.string("location", 128)
+      .notNullable()
+
+    offers.string("time", 128)
+      .notNullable()
+
+    offers.integer("user_id")
+      .notNullable()
+      .references("user_id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE")
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("offers")
 };
