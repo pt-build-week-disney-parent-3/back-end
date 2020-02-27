b5db3185f9ceb03192024beffdf6e597286e766b
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-27 13:30:08 -0500

    Verify Contractors Middleware

--- middleware/verify-contractors.js
+++ middleware/verify-contractors.js
@@ -6,9 +6,74 @@ module.exports = {
 }
 
 function validateContractorId() {
-
+  return (req, res, next) => {
+    Contractors.findById(req.params.id)
+      .then(contractor => {
+        if (contractor) {
+          req.contractor = contractor;
+          next();
+        } else {
+          res.status(400).json({
+            message: "Invalid Contractor Id."
+          })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error while trying to find the contractor. Please try again later."
+        })
+      })
+  }
 }
 
 function validateContractorPost() {
-  
+  return (req, res, next) => {
+    if (!req.body.username) {
+      return res.status(400).json({
+        message: "Please provide a username for your contractor."
+      })
+    } else if (!req.body.password) {
+      return res.status(400).json({
+        message: "Please provide a password for your contractor."
+      })
+    } else if (!req.body.first_name) {
+      return res.status(400).json({
+        message: "Please provide a first name for your contractor."
+      })
+    } else if (!req.body.last_name) {
+      return res.status(400).json({
+        message: "Please provide a last name for your contractor."
+      })
+    } else if (!req.body.email) {
+      return res.status(400).json({
+        message: "Please provide an email for your contractor."
+      })
+    } else if (!req.body.dob) {
+      return res.status(400).json({
+        message: "Please provide a date of birth for your contractor."
+      })
+    } else if (!req.body.phone_number) {
+      return res.status(400).json({
+        message: "Please provide a phone number for your contractor."
+      })
+    } else if (!req.body.cpr_cert) {
+      return res.status(400).json({
+        message: "Please provide a CPR Certification status for your contractor."
+      })
+    } else if (!req.body.type) {
+      return res.status(400).json({
+        message: "Please provide the type, contractor for your contractor."
+      })
+    } else if (!req.body.price) {
+      return res.status(400).json({
+        message: "Please provide a price for your contractor."
+      })
+    } else if (!req.body.user_id) {
+      return res.status(400).json({
+        message: "Please provide a user id for your contractor."
+      })
+    } else {
+      next();
+    }
+  }
 }
