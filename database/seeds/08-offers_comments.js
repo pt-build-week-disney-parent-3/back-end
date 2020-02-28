21b5cd74780443389edf692501914941f4b2a484
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-02-27 21:30:14 -0500

    Offer Comments Seed Created

---
+++ database/seeds/08-offers_comments.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+  return knex("offer_comments").insert([
+    {
+      username: "tiffany87",
+      first_name: "Tiffany",
+      last_name: "Simionescu",
+      offer_comment: "I have one child that I need babysitting for.",
+      offer_id: 1,
+      user_id: 1
+    }
+  ])
+}
