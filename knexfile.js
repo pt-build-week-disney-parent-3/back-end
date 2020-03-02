2ecb1bc86e4ef0185ce2d3216f558721785e1925
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-03-02 14:23:57 -0500

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -46,6 +46,56 @@
 //   },
 // }};
 
+// const sqlite = {
+//   client: "sqlite3",
+//   useNullAsDefault: true,
+//   migrations: {
+//     directory: "./database/migrations",
+//   },
+//   seeds: {
+//     directory: "./database/seeds",
+//   },
+//   pool: {
+//     afterCreate: (conn, done) => {
+//       conn.run('PRAGMA foreign_keys = ON', done);
+//     }
+//   },
+// }
+
+// module.exports = {
+//   dev: {
+//     ...sqlite,
+//     connection: {
+//       filename: "./database/dev.db3"
+//     },
+//   },
+//   test: {
+//     ...sqlite,
+//     connection: {
+//       filename: "./database/test.db3"
+//     }
+//   },
+//   production: {
+//     client: "pg",
+//     connection: {
+//       database: process.env.DB_URL,
+//       user: process.env.USER,
+//       password: process.env.PASSWORD,
+//     },
+//     migrations: {
+//       directory: "./database/migrations",
+//     },
+//     seeds: {
+//       directory: "./database/seeds",
+//     },
+//     pool: {
+//       afterCreate: (conn, done) => {
+//         conn.run('PRAGMA foreign_keys = ON', done);
+//       }
+//     }
+//   }
+// }
+
 const sqlite = {
   client: "sqlite3",
   useNullAsDefault: true,
@@ -69,29 +119,20 @@ module.exports = {
       filename: "./database/dev.db3"
     },
   },
+
   test: {
     ...sqlite,
     connection: {
       filename: "./database/test.db3"
+    },
+  },
+
+  production: {
+    ...sqlite,
+    connection: {
+      database: process.env.DB_URL || "https://disney-parent-3.herokuapp.com/",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     }
   },
-  production: {
-    client: "postgresql",
-    connection: {
-      database: process.env.DB_URL,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-    },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
-    }
-  }
 }
