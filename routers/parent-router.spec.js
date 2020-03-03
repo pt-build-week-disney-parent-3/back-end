b0d00c7937b03512b04064efdc9293c801649f8f
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-03-02 23:47:21 -0500

    Parent Router Tests work

---
+++ routers/parent-router.spec.js
@@ -0,0 +1,28 @@
+const supertest = require('supertest');
+const router = require('./parent-router');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+beforeEach(async () => {
+  await db.seed.run();
+})
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+// GET - /api/parents
+test("get all parents", async () => {
+  const res = await supertest(server.use(router))
+    .get("/api/parents")
+  expect(res.status).toBe(400)
+  expect(res.type).toBe("application/json")
+})
+
+// GET - /api/parents/:id
+test("get a single parents", async () => {
+  const res = await supertest(server.use(router))
+    .get("/api/parents/1")
+  expect(res.status).toBe(400)
+  expect(res.type).toBe("application/json")
+})
