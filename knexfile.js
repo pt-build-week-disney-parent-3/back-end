24f524fea73726e65c750c2481d5bed41de1a134
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-03-02 23:13:04 -0500

    Auth Router Testing works

--- knexfile.js
+++ knexfile.js
@@ -1,100 +1,6 @@
-// // // --env=dev
-// // // --env=test
-
-
-// module.exports = {
-//   development: {
-//     client: "sqlite3",
-//     useNullAsDefault: true,
-//     connection: process.env.DB_URL || "./database/dev.db3",
-//     migrations: {
-//       directory: './database/migrations',
-//     },
-//     seeds: { directory: './database/seeds' },
-//     pool: {
-//       afterCreate: (conn, done) => {
-//       conn.run('PRAGMA foreign_keys = ON', done);
-//     },
-//   },
-//   },
-
-//   testing: {
-//     client: 'pg',
-//     connection: process.env.DB_URL || "./database/test.db3",
-//     migrations: {
-//       directory: './database/migrations',
-//     },
-//     seeds: { directory: './database/seeds' },
-//     pool: {
-//       afterCreate: (conn, done) => {
-//       conn.run('PRAGMA foreign_keys = ON', done);
-//     },
-//   },
-//   },
-
-//   production: {
-//     client: 'pg',
-//     connection: process.env.DB_URL || "./database/dev.db3",
-//     migrations: {
-//       directory: './database/migrations',
-//     },
-//     seeds: { directory: './database/seeds' },
-//     pool: {
-//       afterCreate: (conn, done) => {
-//       conn.run('PRAGMA foreign_keys = ON', done);
-//     },
-//   },
-// }};
-
-// const sqlite = {
-//   client: "sqlite3",
-//   useNullAsDefault: true,
-//   migrations: {
-//     directory: "./database/migrations",
-//   },
-//   seeds: {
-//     directory: "./database/seeds",
-//   },
-//   pool: {
-//     afterCreate: (conn, done) => {
-//       conn.run('PRAGMA foreign_keys = ON', done);
-//     }
-//   },
-// }
-
-// module.exports = {
-//   dev: {
-//     ...sqlite,
-//     connection: {
-//       filename: "./database/dev.db3"
-//     },
-//   },
-//   test: {
-//     ...sqlite,
-//     connection: {
-//       filename: "./database/test.db3"
-//     }
-//   },
-//   production: {
-//     client: "pg",
-//     connection: {
-//       database: process.env.DB_URL,
-//       user: process.env.USER,
-//       password: process.env.PASSWORD,
-//     },
-//     migrations: {
-//       directory: "./database/migrations",
-//     },
-//     seeds: {
-//       directory: "./database/seeds",
-//     },
-//     pool: {
-//       afterCreate: (conn, done) => {
-//         conn.run('PRAGMA foreign_keys = ON', done);
-//       }
-//     }
-//   }
-// }
+// --env=dev
+// --env=test
+// --env=production
 
 const sqlite = {
   client: "sqlite3",
@@ -130,10 +36,6 @@ module.exports = {
   production: {
     ...sqlite,
     connection: {
-      // database: "https://disney-parent-3.herokuapp.com/",
-      // user: process.env.DB_USER,
-      // password: process.env.DB_PASSWORD
-      // filename: "https://disney-parent-3.herokuapp.com/"
       filename: "./database/production.db3"
     }
   },
--- routers/answer-router.spec.js
+++ routers/answer-router.spec.js
@@ -1,5 +1,6 @@
 const supertest = require('supertest');
 const router = require('./answer-router');
+const authRouter = require('./auth-router');
 const server = require('../api/server');
 const db = require('../database/dbConfig');
 
@@ -7,35 +8,153 @@ beforeEach(async () => {
   await db.seed.run();
 })
 
-let token;
-
-beforeAll((done) => {
-  supertest(server)
-    .post('/api/auth/login/parent')
-    .send({
-      username: "tiffany87",
-      password: "123456",
-    })
-    .end((err, response) => {
-      token = response.body.token; // save the token!
-      done();
-    });
+afterAll(async () => {
+  await db.destroy();
 });
+
+// Parents
+// beforeAll((done) => {
+//   supertest(server.use(authRouter))
+//     .post("/api/auth/login/contractor")
+//     .send({
+//       username: "tiffany94",
+//       password: "secret"
+//     })
+//     .end((err, res) => {
+//       token = res.body.token;
+//       done();
+//     })
+// })
+
+// describe("answer router tests", () => {
+
+//   // GET - /api/answers/question/:id 
+//   test("get all answers to a question", () => {
+//     return supertest(server.use(router))
+//       .get("/api/answers/question/1")
+//       .set("authorization", `${token}`)
+//       .then((res => {
+//         expect(res.status).toBe(403)
+//         expect(res.type).toBe("application/json")
+//       }))
+//   })
+
+  // // GET - /api/answers/:id
+  // test("get single answer", () => {
+  //   return supertest(server.use(router))
+  //     .get("/api/answers/1")
+  //     .set("authorization", `${token}`)
+  //     .then((res => {
+  //       expect(res.status).toBe(400)
+  //       expect(res.type).toBe("application/json")
+  //     }))
+  // })
+
+  // // POST - /api/answers
+  // test("get single answer", () => {
+  //   return supertest(server.use(router))
+  //     .post("/api/answers")
+  //     .send({
+  //       username: "tiffany93",
+  //       first_name: "Tiffany",
+  //       last_name: "Simionescu",
+  //       answer: "My test answer",
+  //       question_id: 1,
+  //     })
+  //     .set("authorization", `${token}`)
+  //     .then((res => {
+  //       expect(res.status).toBe(400)
+  //       expect(res.type).toBe("application/json")
+  //     }))
+  // })
+
+  // // PUT - /api/answers/:id
+  // test("update single answer", () => {
+  //   return supertest(server.use(router))
+  //     .put("/api/answers/1")
+  //     .send({
+  //       username: "tiffany93",
+  //       first_name: "Tiffany",
+  //       last_name: "Simionescu",
+  //       answer: "My updated test answer",
+  //       question_id: 1,
+  //     })
+  //     .set("authorization", `${token}`)
+  //     .then((res => {
+  //       expect(res.status).toBe(400)
+  //       expect(res.type).toBe("application/json")
+  //     }))
+  // })
+
+  // // DELETE - /api/answers/:id
+  // test("delete single answer", () => {
+  //   return supertest(server.use(router))
+  //     .delete("/api/answers/2")
+  //     .set("authorization", `${token}`)
+  //     .then((res => {
+  //       expect(res.status).toBe(400)
+  //       expect(res.type).toBe("application/json")
+  //     }))
+  // })
+// })
 
 // GET - /api/answers/question/:id
 test("get all answers to a question", async () => {
-  // let token = res.body.token;
   const res = await supertest(server.use(router))
     .get("/api/answers/question/1")
-    .set("authorization", `${token}`)
-  expect(res.status).toBe(403)
+    // .set("jwtSecret", "My favorite park is Epcot")
+  expect(res.status).toBe(400)
   expect(res.type).toBe("application/json")
 })
 
 // GET - /api/answers/:id
+test("get single answer", async () => {
+  const res = await supertest(server.use(router))
+    .get("/api/answers/1")
+    // .set("authorization", `${token}`)
+  expect(res.status).toBe(400)
+  expect(res.type).toBe("application/json")
+})
 
-// POST - /api/answers
+// // POST - /api/answers
+// test("add answer with no req.body", async () => {
+//   const res = await supertest(server.use(router))
+//     .post("/api/answers/")
+//     .send({ 
+//       username: "tiffany88",
+//       first_name: "Tiffany",
+//       last_name: "Lynn",
+//       answer: "My Answer",
+//       question_id: 1,
+//       contractor_id: 1
+//     })
+//     // .set("authorization", `${token}`)
+//   expect(res.status).toBe(403)
+//   expect(res.type).toBe("application/json")
+// })
 
-// PUT - /api/answers/:id
+// // PUT - /api/answers/:id
+// test("get single answer - no req.body, wrong id", async () => {
+//   const res = await supertest(server.use(router))
+//     .put("/api/answers/1")
+//     // .set("authorization", `${token}`)
+//     .send({ 
+//       username: "tiffany88",
+//       first_name: "Tiffany",
+//       last_name: "Lynn",
+//       answer: "My New Answer",
+//       question_id: 1,
+//       contractor_id: 1
+//     })
+//   expect(res.status).toBe(403)
+//   expect(res.type).toBe("application/json")
+// })
 
-// DELETE - /api/answers/:id
+// // DELETE - /api/answers/:id
+// test("delete answer - id doesn't exist", async () => {
+//   const res = await supertest(server.use(router))
+//     .delete("/api/answers/100")
+//     // .set("authorization", `${token}`)
+//   expect(res.status).toBe(403)
+//   expect(res.type).toBe("application/json")
+// })
