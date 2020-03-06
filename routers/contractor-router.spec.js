9c990f803d2deae37b2cf2225bf83d731836f1c4
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-03-06 02:04:39 -0500

    Contractor Router test works

--- routers/contractor-router.spec.js
+++ routers/contractor-router.spec.js
@@ -1,22 +1,11 @@
 const supertest = require('supertest');
 const router = require('./contractor-router');
 const server = require('../api/server');
-const db = require('../database/dbConfig');
-
-beforeEach(async () => {
-  await db.migrate.rollback();
-  await db.migrate.latest();
-  await db.seed.run();
-})
-
-afterEach(async () => {
-  await db.destroy();
-});
 
 // GET - /api/contractors
 test("get all contractors", async () => {
   const res = await supertest(server.use(router))
-    .get("/api/contractors")
+    .get("/")
   expect(res.status).toBe(200)
   expect(res.type).toBe("application/json")
 })
@@ -24,7 +13,26 @@ test("get all contractors", async () => {
 // GET - /api/contractors/:id
 test("get a single contractor", async () => {
   const res = await supertest(server.use(router))
-    .get("/api/contractor/1")
+    .get("/1")
   expect(res.status).toBe(200)
-  expect(res.type).toBe("text/html")
+  expect(res.type).toBe("application/json")
+})
+
+// PUT - /api/contractors/:id
+test("update contractor", async () => {
+  const res = await supertest(server.use(router))
+    .put("/1")
+    .send({
+      username: "tiffany88",
+      password: "123456",
+      first_name: "Tiffany",
+      last_name: "NewLastName",
+      email: "e@r.com",
+      dob: "September",
+      phone_number: "0099332211",
+      cpr_cert: true,
+      price: "$20.00"
+    })
+  expect(res.status).toBe(201)
+  expect(res.type).toBe("application/json")
 })
