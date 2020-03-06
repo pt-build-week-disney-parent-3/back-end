20e86c6387bd6ff581ba598ed44f44d18eb8ef37
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-03-06 02:18:06 -0500

    Parent Router test works

--- routers/parent-router.spec.js
+++ routers/parent-router.spec.js
@@ -1,30 +1,37 @@
 const supertest = require('supertest');
 const router = require('./parent-router');
 const server = require('../api/server');
-const db = require('../database/dbConfig');
-
-beforeEach(async () => {
-  await db.migrate.rollback();
-  await db.migrate.latest();
-  await db.seed.run();
-})
-
-afterEach(async () => {
-  await db.destroy();
-});
 
 // GET - /api/parents
 test("get all parents", async () => {
   const res = await supertest(server.use(router))
-    .get("/api/parents")
+    .get("/")
   expect(res.status).toBe(200)
   expect(res.type).toBe("application/json")
 })
 
 // GET - /api/parents/:id
-test("get a single parents", async () => {
+test("get a single parent", async () => {
   const res = await supertest(server.use(router))
-    .get("/api/parents/1")
+    .get("/1")
   expect(res.status).toBe(200)
+  expect(res.type).toBe("application/json")
+})
+
+// PUT - /api/parents/:id
+test("update a parent", async () => {
+  const res = await supertest(server.use(router))
+    .put("/1")
+    .send({
+      username: "tiffany87",
+      password: "123456",
+      first_name: "Tiffany",
+      last_name: "Simionescu",
+      email: "lynnts@gmail.com",
+      dob: "September",
+      phone_number: "8675309",
+      cpr_cert: true
+    })
+  expect(res.status).toBe(201)
   expect(res.type).toBe("application/json")
 })
