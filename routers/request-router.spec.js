ce06c47f809717026279606fe869197f2b67ece5
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-03-06 02:29:51 -0500

    Request Router test works

--- routers/request-router.spec.js
+++ routers/request-router.spec.js
@@ -1,22 +1,59 @@
 const supertest = require('supertest');
 const router = require('./request-router');
 const server = require('../api/server');
-const db = require('../database/dbConfig');
-
-beforeEach(async () => {
-  await db.migrate.rollback();
-  await db.migrate.latest();
-  await db.seed.run();
-})
-
-afterEach(async () => {
-  await db.destroy();
-});
 
 // GET - /api/requests/parent/:id
 test("get all requests for a single parent", async () => {
   const res = await supertest(server.use(router))
-    .get("/api/requests/parent/1")
+    .get("/parent/1")
+  expect(res.status).toBe(200)
+  expect(res.type).toBe("application/json")
+})
+
+// POST - /api/requests
+test("add a request", async () => {
+  const res = await supertest(server.use(router))
+    .post("/")
+    .send({
+      username: "tiffany87",
+      first_name: "Tiffany",
+      last_name: "Simionescu",
+      request: "This is my request",
+      child_count: "2",
+      location: "Magic Kingdom",
+      time: "2pm",
+      stroller: false,
+      ride: "Spalsh Mountain",
+      parent_id: 1
+    })
+  expect(res.status).toBe(201)
+  expect(res.type).toBe("application/json")
+})
+
+// PUT - /api/requests/:id
+test("update a request", async () => {
+  const res = await supertest(server.use(router))
+    .put("/2")
+    .send({
+      username: "tiffany87",
+      first_name: "Tiffany",
+      last_name: "Simionescu",
+      request: "This is my request updated",
+      child_count: "2",
+      location: "Magic Kingdom",
+      time: "2pm",
+      stroller: false,
+      ride: "Spalsh Mountain",
+      parent_id: 1
+    })
+  expect(res.status).toBe(201)
+  expect(res.type).toBe("application/json")
+})
+
+// DELETE - /api/requests/:id
+test("remove a request", async () => {
+  const res = await supertest(server.use(router))
+    .delete("/2")
   expect(res.status).toBe(200)
   expect(res.type).toBe("application/json")
 })
