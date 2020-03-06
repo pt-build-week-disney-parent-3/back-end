097ebe93b32025248c1608e98655ecbecc746875
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-03-06 02:21:38 -0500

    Question Router test works

--- routers/question-router.spec.js
+++ routers/question-router.spec.js
@@ -1,22 +1,11 @@
 const supertest = require('supertest');
 const router = require('./question-router');
 const server = require('../api/server');
-const db = require('../database/dbConfig');
-
-beforeEach(async () => {
-  await db.migrate.rollback();
-  await db.migrate.latest();
-  await db.seed.run();
-})
-
-afterEach(async () => {
-  await db.destroy();
-});
 
 // GET - /api/questions
 test("get all questions", async () => {
   const res = await supertest(server.use(router))
-    .get("/api/questions")
+    .get("/")
   expect(res.status).toBe(200)
   expect(res.type).toBe("application/json")
 })
@@ -24,7 +13,45 @@ test("get all questions", async () => {
 // GET - /api/questions/parent/:id
 test("get all questions for a single parent", async () => {
   const res = await supertest(server.use(router))
-    .get("/api/questions/parent/1")
+    .get("/parent/1")
+  expect(res.status).toBe(200)
+  expect(res.type).toBe("application/json")
+})
+
+// POST - /api/questions
+test("add a question", async () => {
+  const res = await supertest(server.use(router))
+    .post("/")
+    .send({
+      username: "tiffany87",
+      first_name: "Tiffany",
+      last_name: "Simionescu",
+      question: "This is my question",
+      parent_id: 1
+    })
+  expect(res.status).toBe(201)
+  expect(res.type).toBe("application/json")
+})
+
+// PUT - /api/questions/:id
+test("update a question", async () => {
+  const res = await supertest(server.use(router))
+    .put("/2")
+    .send({
+      username: "tiffany87",
+      first_name: "Tiffany",
+      last_name: "Simionescu",
+      question: "This is my question updated",
+      parent_id: 1
+    })
+  expect(res.status).toBe(201)
+  expect(res.type).toBe("application/json")
+})
+
+// DELETE - /api/question/:id
+test("remove a question", async () => {
+  const res = await supertest(server.use(router))
+    .delete("/2")
   expect(res.status).toBe(200)
   expect(res.type).toBe("application/json")
 })
